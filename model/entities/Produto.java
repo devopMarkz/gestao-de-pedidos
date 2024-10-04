f0380113b1f02783827d95e8818c3f0fa5a1bac0
devopMarkz <marcosdev2002@gmail.com>
2024-10-04 10:03:48 -0300

    Criando entidade Pedido

---
+++ model/entities/Produto.java
@@ -0,0 +1,24 @@
+package model.entities;
+
+public class Produto {
+	
+	private String nome;
+	
+	public Produto(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produto: " + nome;
+	}
+
+}
