9e3c2e2cf6f2bba83d44e84d29bcbeddb06294fc
devopMarkz <marcosdev2002@gmail.com>
2024-10-04 13:31:05 -0300

    Criando entidade abstrata Pedido

---
+++ model/entities/Pedido.java
@@ -0,0 +1,92 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import model.entities.enums.StatusPedido;
+
+public abstract class Pedido {
+	
+	private Integer numeroDoPedido;
+	private LocalDate dataPedido;
+	private List<Item> itens;
+	private Cliente cliente;
+	private StatusPedido statusPedido;
+	
+	public Pedido() {
+	}
+	
+	// CONSTRUTOR GERAL PARA RECEBER DE ARQUIVOS O OBJETO
+	public Pedido(Integer numeroDoPedido, LocalDate dataPedido, List<Item> itens, Cliente cliente, StatusPedido statusPedido) {
+		this.numeroDoPedido = numeroDoPedido;
+		this.dataPedido = dataPedido;
+		this.itens = itens;
+		this.cliente = cliente;
+		this.statusPedido = statusPedido;
+	}
+	
+	// GETTERS E SETTERS
+
+	public List<Item> getItens() {
+		return itens;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public StatusPedido getStatusPedido() {
+		return statusPedido;
+	}
+
+	public void setStatusPedido(StatusPedido statusPedido) {
+		this.statusPedido = statusPedido;
+	}
+
+	public Integer getNumeroDoPedido() {
+		return numeroDoPedido;
+	}
+
+	public LocalDate getDataPedido() {
+		return dataPedido;
+	}
+	
+	// MÉTODOS DA CLASSE
+	
+	public void adicionarItem(Item item) {
+		this.getItens().add(item);
+	}
+	
+	public abstract Double calcularValorTotal();
+	
+	public abstract void gerarRelatorio();
+	
+}
+
+
+
+
+
+
+
+
+//// CONSTRUTOR PARA PEDIDO EM_PROCESSAMENTO 
+//public Pedido(Integer numeroDoPedido, Cliente cliente) {
+//	this.numeroDoPedido = numeroDoPedido;
+//	this.dataPedido = LocalDate.now();
+//	this.cliente = cliente;
+//	this.statusPedido = StatusPedido.EM_PROCESSAMENTO;
+//}
+//
+//// CONSTRUTOR PARA PEDIDO ENVIADO
+//public Pedido(Integer numeroDoPedido, Cliente cliente, List<Item> itens) {
+//	this.numeroDoPedido = numeroDoPedido;
+//	this.itens = itens;
+//	this.cliente = cliente;
+//	this.statusPedido = StatusPedido.ENVIADO;
+//}
+//
--- model/entities/enums/StatusPedido.java
+++ model/entities/enums/StatusPedido.java
@@ -1,4 +1,4 @@
-package model.entities;
+package model.entities.enums;
 
 public enum StatusPedido {
 	
